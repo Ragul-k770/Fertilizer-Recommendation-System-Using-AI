1485a2636fccda9f992ca8133229f189120a91e4
Ragul-k770 <ragulkrishnan335@gmail.com>
2026-01-19 22:08:08 +0530

    Added SoilData class

---
+++ Source_Code/Source_Code/Source_Code/SoilData.java
@@ -0,0 +1,16 @@
+public class SoilData {
+
+    int nitrogen;
+    int phosphorus;
+    int potassium;
+    double ph;
+    String crop;
+
+    public SoilData(int nitrogen, int phosphorus, int potassium, double ph, String crop) {
+        this.nitrogen = nitrogen;
+        this.phosphorus = phosphorus;
+        this.potassium = potassium;
+        this.ph = ph;
+        this.crop = crop;
+    }
+}
